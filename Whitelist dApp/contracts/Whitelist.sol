05b48a387684bb9a56b0be8a090c5049233567ad
nslee333 <nslee333@gmail.com>
2022-03-05 14:01:57 -0800

    Commit

--- Whitelist dApp/contracts/Whitelist.sol
+++ Whitelist dApp/contracts/Whitelist.sol
@@ -19,4 +19,13 @@ contract Whitelist {
     constructor(uint8 _maxWhitelistedAddresses) {
         maxWhitelistedAddresses = _maxWhitelistedAddresses;
     }
+
+
+    function addAddressToWhitelist() public {
+        // msg.sender is the address of the user who called this function.
+        require(!whitelistedAddresses[msg.sender], "You are already in Whitelist.")
+        require(numAddressesWhitelisted < maxWhitelistedAddresses, "The Maximum Whitelisted Users has been Reached");
+        whitelistedAddresses[msg.sender] = true;
+        numAddressesWhitelisted += 1;
+    }
 }
