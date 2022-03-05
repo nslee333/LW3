d16ba6fa1122e23cf45edf41d5e21f6340e45bee
nslee333 <nslee333@gmail.com>
2022-03-04 19:05:45 -0800

    Commit

---
+++ Whitelist dApp/contracts/Whitelist.sol
@@ -0,0 +1,21 @@
+// SPDX-license-Idenitifer: MIT
+
+pragma solidity ^0.8.0;
+
+contract Whitelist {
+
+    // Maximum number of addresses that can be whitelisted.
+    uint8 public maxWhitelistedAddresses;
+
+
+    // Current number of addresses whitelisted.
+    uint8 public numAddressesWhitelisted;
+
+    // Mappping of addresses whitelisted.
+    mapping(address => bool) public whitelistedAddresses;
+
+
+    constructor(uint8 _maxWhitelistedAddresses) {
+        maxWhitelistedAddresses = _maxWhitelistedAddresses;
+    }
+}
