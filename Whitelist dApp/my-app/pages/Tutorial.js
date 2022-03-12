11bac0bf76cc49b18b8d5171d80ad7c05a94dd72
nslee333 <nslee333@gmail.com>
2022-03-11 20:58:56 -0800

    Commit

---
+++ Whitelist dApp/my-app/pages/Tutorial.js
@@ -0,0 +1,24 @@
+import Head from "next/head";
+import Image from 'next/image'
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+    return {
+    <div>
+        <Head>
+            <title> Whitelist dApp </title>
+            <meta name="description" content="Whitelist-Dapp" />
+        </Head>
+        <div className={styles.main}>
+            <h1 className={styles.title}>
+            
+            </h1>
+        </div>
+
+        <footer>
+
+        </footer>
+
+    </div>
+    }
+}
