d8bc03f10066184bbe0611287faa539ad8232b76
nslee333 <nslee333@gmail.com>
2022-03-14 19:40:51 -0700

    Commit

--- Whitelist dApp/my-app/pages/index.js
+++ Whitelist dApp/my-app/pages/index.js
@@ -1,12 +1,25 @@
 import Head from "next/head";
 import Image from 'next/image';
 import styles from "../styles/Home.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-
+    const [walletConnected, setWalletConnected] = useState(false);
     const [numOfWhitelisted, setNumOfWhitelisted] = useState(0);
     //  This is linking up the numOfWhitelisted to the react state. 
+    const web3ModalRef = useRef();
+
+
+    useEffect(() => {
+        if (!walletConnected) {{
+            web3ModalRef.current = new web3Modal({
+                network: "rinkeby",
+                providerOptions: {},
+                disabledInjectedProvider: false,
+            });
+            connectWallet();
+        }}
+    }) [walletConnected]
 
     return (
             <div>
@@ -15,11 +28,13 @@ export default function Home() {
                             <meta name="description" content="Whitelist-Dapp" />
                     </Head>
                     <div className={styles.main}>
+                    
                         <h1 className={styles.title}>
                         Welcome to Crypto Devs!
                         </h1>
+                        
                             <div className={styles.description}>
-                                {numOfWhitelisted} have already joined the Whitelist.
+                            {numOfWhitelisted} have already joined the Whitelist.
                             </div>
                         <div>
                             <img className={styles.image} src="./crypto-devs.svg" />
