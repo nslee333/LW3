d55ecee90cdd27a4db41a707bc5bb8e59242be06
nslee333 <nslee333@gmail.com>
2022-03-15 17:41:56 -0700

    Commit

--- Whitelist dApp/my-app/pages/index.js
+++ Whitelist dApp/my-app/pages/index.js
@@ -1,14 +1,62 @@
 import Head from "next/head";
 import Image from 'next/image';
 import styles from "../styles/Home.module.css";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import Web3Modal from "web3modal"; 
+import { providers } from "ethers";
 
 export default function Home() {
     const [walletConnected, setWalletConnected] = useState(false);
     const [numOfWhitelisted, setNumOfWhitelisted] = useState(0);
     //  This is linking up the numOfWhitelisted to the react state. 
     const web3ModalRef = useRef();
+    
+    
+    
+    const getProviderOrSigner = async(needSigner = false) => {
+        try {
+            const provider = await web3ModalRef.current.connect();
+            const web3Provider = new providers.Web3Provider(provider);
+            // This current.connect from web3Modal returns the provider.
+
+            const {chainId} = await web3Provider.getNetwork();
+            if(chainId !== 4) {
+                window.alert("Change the network to Rinkeby");
+                throw new Error("Change the network to Rinkeby");
+            }
+            if(needSigner) {
+                const signer = web3Provider.getSigner();
+                return signer;
+            }
+            return web3Provider;
+        } catch(err) {
+            console.error(err);
+        }
+    }
+
+
+
+
+    const checkIfAddressIsWhitelisted = async ZZZZ
+
+
+
+
+    const getNumberOfWhitelisted = async zzzzzz
+    
+
+
+    
+    const connectWallet = async() => {
+        try {
+            await getProviderOrSigner();
+            setWalletConnected(true)
+            checkIfAddressIsWhitelisted();
+            getNumberOfWhitelisted();
+        } catch(err) {
+            console.error(err)
+        }
+    }
 
 
     useEffect(() => {
