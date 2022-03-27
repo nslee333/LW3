cefb9414cc84ea070ce5243808b7086e0c99f224
nslee333 <nslee333@gmail.com>
2022-03-27 11:49:20 -0700

    Commit

--- NFT-Collection/contract backend/contracts/Cryptodevs.sol
+++ NFT-Collection/contract backend/contracts/Cryptodevs.sol
@@ -1,6 +1,18 @@
 // SPDX-licence-Idenitifier: MIT
 pragma solidity 0.8.0;
 
-import '@openzeppelin/contracts/ERC721';
+import '@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+import './IWhitelist.sol';
 
-contract DevsNFT is ERC-721
+contract CryptoDevs is ERC721Enumerable, Ownable {
+    
+    string _baseTokenURI;
+    IWhitelist whitelist;
+
+    constructor(string memory _baseURI, address whitelistContract) ERC721("Crypto Devs", "CD") {
+       _baseTokenURI = _baseURI;
+       whitelist = IWhitelist(whitelistContract);
+    }
+
+}
