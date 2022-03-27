dd3ee5420480378ecfec644fcda4aa3229743b03
nslee333 <nslee333@gmail.com>
2022-03-27 00:13:30 -0700

    Commit

---
+++ NFT-Collection/contract backend/ownable.sol
@@ -0,0 +1,6 @@
+// SPDX-licence-Idenitifier: MIT
+pragma solidity 0.8.0;
+
+import '@openzeppelin/contracts/ERC721';
+
+contract DevsNFT is ERC-721
---
+++ zNotes/ERC-721.py
@@ -0,0 +1,152 @@
+from web3 import Web3
+from web3._utils.events import get_event_data
+
+
+w3 = Web3(Web3.HTTPProvider("https://cloudflare-eth.com"))
+
+ck_token_addr = "0x06012c8cf97BEaD5deAe237070F9587f8E7A266d"    # CryptoKitties Contract
+
+acc_address = "0xb1690C08E213a35Ed9bAb7B318DE14420FB57d8C"      # CryptoKitties Sales Auction
+
+# This is a simplified Contract Application Binary Interface (ABI) of an ERC-721 NFT Contract.
+# It will expose only the methods: balanceOf(address), name(), ownerOf(tokenId), symbol(), totalSupply()
+simplified_abi = [
+    {
+        'inputs': [{'internalType': 'address', 'name': 'owner', 'type': 'address'}],
+        'name': 'balanceOf',
+        'outputs': [{'internalType': 'uint256', 'name': '', 'type': 'uint256'}],
+        'payable': False, 'stateMutability': 'view', 'type': 'function', 'constant': True
+    },
+    {
+        'inputs': [],
+        'name': 'name',
+        'outputs': [{'internalType': 'string', 'name': '', 'type': 'string'}],
+        'stateMutability': 'view', 'type': 'function', 'constant': True
+    },
+    {
+        'inputs': [{'internalType': 'uint256', 'name': 'tokenId', 'type': 'uint256'}],
+        'name': 'ownerOf',
+        'outputs': [{'internalType': 'address', 'name': '', 'type': 'address'}],
+        'payable': False, 'stateMutability': 'view', 'type': 'function', 'constant': True
+    },
+    {
+        'inputs': [],
+        'name': 'symbol',
+        'outputs': [{'internalType': 'string', 'name': '', 'type': 'string'}],
+        'stateMutability': 'view', 'type': 'function', 'constant': True
+    },
+    {
+        'inputs': [],
+        'name': 'totalSupply',
+        'outputs': [{'internalType': 'uint256', 'name': '', 'type': 'uint256'}],
+        'stateMutability': 'view', 'type': 'function', 'constant': True
+    },
+]
+
+ck_extra_abi = [
+    {
+        'inputs': [],
+        'name': 'pregnantKitties',
+        'outputs': [{'name': '', 'type': 'uint256'}],
+        'payable': False, 'stateMutability': 'view', 'type': 'function', 'constant': True
+    },
+    {
+        'inputs': [{'name': '_kittyId', 'type': 'uint256'}],
+        'name': 'isPregnant',
+        'outputs': [{'name': '', 'type': 'bool'}],
+        'payable': False, 'stateMutability': 'view', 'type': 'function', 'constant': True
+    }
+]
+
+ck_contract = w3.eth.contract(address=w3.toChecksumAddress(ck_token_addr), abi=simplified_abi+ck_extra_abi)
+name = ck_contract.functions.name().call()
+symbol = ck_contract.functions.symbol().call()
+kitties_auctions = ck_contract.functions.balanceOf(acc_address).call()
+print(f"{name} [{symbol}] NFTs in Auctions: {kitties_auctions}")
+
+pregnant_kitties = ck_contract.functions.pregnantKitties().call()
+print(f"{name} [{symbol}] NFTs Pregnants: {pregnant_kitties}")
+
+# Using the Transfer Event ABI to get info about transferred Kitties.
+tx_event_abi = {
+    'anonymous': False,
+    'inputs': [
+        {'indexed': False, 'name': 'from', 'type': 'address'},
+        {'indexed': False, 'name': 'to', 'type': 'address'},
+        {'indexed': False, 'name': 'tokenId', 'type': 'uint256'}],
+    'name': 'Transfer',
+    'type': 'event'
+}
+
+# We need the event's signature to filter the logs
+event_signature = w3.sha3(text="Transfer(address,address,uint256)").hex()
+
+logs = w3.eth.getLogs({
+    "fromBlock": w3.eth.blockNumber - 120,
+    "address": w3.toChecksumAddress(ck_token_addr),
+    "topics": [event_signature]
+})
+
+# Notes:
+#   - 120 blocks is the max range for CloudFlare Provider
+#   - If you didn't find any Transfer event you can also try to get a tokenId at:
+#       https://etherscan.io/address/0x06012c8cf97BEaD5deAe237070F9587f8E7A266d#events
+#       Click to expand the event's logs and copy its "tokenId" argument
+
+recent_tx = [get_event_data(w3.codec, tx_event_abi, log)["args"] for log in logs]
+
+kitty_id = recent_tx[0]['tokenId'] # Paste the "tokenId" here from the link above
+is_pregnant = ck_contract.functions.isPregnant(kitty_id).call()
+print(f"{name} [{symbol}] NFTs {kitty_id} is pregnant: {is_pregnant}")
+
+----------------------------------------------------------------------------------------------
+
+# Using the Pregnant and Birth Events ABI to get info about new Kitties.
+ck_extra_events_abi = [
+    {
+        'anonymous': False,
+        'inputs': [
+            {'indexed': False, 'name': 'owner', 'type': 'address'},
+            {'indexed': False, 'name': 'matronId', 'type': 'uint256'},
+            {'indexed': False, 'name': 'sireId', 'type': 'uint256'},
+            {'indexed': False, 'name': 'cooldownEndBlock', 'type': 'uint256'}],
+        'name': 'Pregnant',
+        'type': 'event'
+    },
+    {
+        'anonymous': False,
+        'inputs': [
+            {'indexed': False, 'name': 'owner', 'type': 'address'},
+            {'indexed': False, 'name': 'kittyId', 'type': 'uint256'},
+            {'indexed': False, 'name': 'matronId', 'type': 'uint256'},
+            {'indexed': False, 'name': 'sireId', 'type': 'uint256'},
+            {'indexed': False, 'name': 'genes', 'type': 'uint256'}],
+        'name': 'Birth',
+        'type': 'event'
+    }]
+
+# We need the event's signature to filter the logs
+ck_event_signatures = [
+    w3.sha3(text="Pregnant(address,uint256,uint256,uint256)").hex(),
+    w3.sha3(text="Birth(address,uint256,uint256,uint256,uint256)").hex(),
+]
+
+# Here is a Pregnant Event:
+# - https://etherscan.io/tx/0xc97eb514a41004acc447ac9d0d6a27ea6da305ac8b877dff37e49db42e1f8cef#eventlog
+pregnant_logs = w3.eth.getLogs({
+    "fromBlock": w3.eth.blockNumber - 120,
+    "address": w3.toChecksumAddress(ck_token_addr),
+    "topics": [ck_event_signatures[0]]
+})
+
+recent_pregnants = [get_event_data(w3.codec, ck_extra_events_abi[0], log)["args"] for log in pregnant_logs]
+
+# Here is a Birth Event:
+# - https://etherscan.io/tx/0x3978028e08a25bb4c44f7877eb3573b9644309c044bf087e335397f16356340a
+birth_logs = w3.eth.getLogs({
+    "fromBlock": w3.eth.blockNumber - 120,
+    "address": w3.toChecksumAddress(ck_token_addr),
+    "topics": [ck_event_signatures[1]]
+})
+
+recent_births = [get_event_data(w3.codec, ck_extra_events_abi[1], log)["args"] for log in birth_logs]
