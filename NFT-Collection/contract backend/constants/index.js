10d916c92505c6c53260e52442e6af595b1b1ac4
nslee333 <nslee333@gmail.com>
2022-04-09 10:20:57 -0700

    Commit

--- NFT-Collection/contract backend/constants/index.js
+++ NFT-Collection/contract backend/constants/index.js
@@ -1,5 +1,5 @@
 const WHITELIST_CONTRACT_ADDRESS = "0xba85dF8B688dA830483F47FAc476249499E63ECc";
 
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://n-nft-collection.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
