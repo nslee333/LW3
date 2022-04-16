89233c2e95f71ec67b37c5c5d5deb0d95d7f5ec8
nslee333 <nslee333@gmail.com>
2022-04-16 09:39:47 -0700

    Commit

--- ICO/hardhat-tutorial/contracts/CryptoDevToken.sol
+++ ICO/hardhat-tutorial/contracts/CryptoDevToken.sol
@@ -9,11 +9,35 @@ import "./ICryptoDevs.sol";
 
 contract CryptoDevTokenis ERC20, Ownable {
 
-    ICryptoDevs CryptoDevsNFT;
+    ICryptoDevs CryptoDevsNFT; 
+    uint256 public constant tokenPerNFT = 10 * 10**10;
+    // _mint function from the OZ Erc20 contract takes a big number.
     
-    constructor(address _cryptoDevsContract) ERC20("Crypto Dev Token", "CD") {
-        CryptoDevsNFT = ICryptoDevs(_cryptoDevsContract);
-        
+    mapping(uint256 => bool) public tokenIdsClaimed;
+
+    constructor(address _cryptoDevsContract) ERC20("Crypto Dev Token", "CD") { // We're inputing the cryptoDevs contract for interface.
+        CryptoDevsNFT = ICryptoDevs(_cryptoDevsContract);        
+    }
+
+
+    function claim() public {
+        address sender = msg.sender;
+        uint256 balance = CryptoDevsNFT.balanceOf(sender); 
+        require(balance > 0; "You don't own any Crypto Dev NFT's);
+        uint256 amount = 0;
+
+        for(uint256 i = 0; i < balance; i++) {
+            uint256 tokenId = CryptoDevsNFT.tokenOfOwnerByIndex(sender, i);
+            if(!tokenIdsClaimed[tokenId]) {
+                amount += 1;
+                tokenIdsClaimed[tokenId] = true;
+
+            }
+        }
+        require(amount > 0, "You have already claimed all of your tokens");
+
+        _mint(msg.sender, amount * )
+
     }
 }
 
