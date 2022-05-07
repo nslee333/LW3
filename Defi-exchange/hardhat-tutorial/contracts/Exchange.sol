9e3444cbfbaaf9c34af490b2fced64d25024a772
nslee333 <nslee333@gmail.com>
2022-05-07 13:51:28 -0700

    Commit

--- Defi-exchange/hardhat-tutorial/contracts/Exchange.sol
+++ Defi-exchange/hardhat-tutorial/contracts/Exchange.sol
@@ -17,15 +17,14 @@ contract Exchange is ERC20 {
     // address.
 
     function getReserve() public view returns (uint) {
-        return ERC20(cryptoDevTokenAddress).balanceOf(address(this));
+        return ERC20(cryptoDevTokenAddress).balanceOf(address(this)); // Goes to the mapping in the CryptoDevToken contract, and gets how much tokens this address owns.
     }
-    // This function gets and returns the balance of the the crypto dev tokens contract.
 
     function addLiquidity(uint _amount) public payable returns (uint) {
-        uint liquidity;
+        uint liquidity; 
         uint ethBalance = address(this).balance;
         uint cryptoDevTokenReserve = getReserve();
-        ERC20 cryptoDevToken = ERC20(cryptoDevTokenAddress);
+        ERC20 cryptoDevToken = ERC20(cryptoDevTokenAddress); // ERC20 is a data type, and it's creating an instance of the address? not sure.
         
         
         if(cryptoDevTokenReserve == 0) {
@@ -45,35 +44,41 @@ contract Exchange is ERC20 {
             _mint(msg.sender, liquidity);
         } 
         // Else, Check that the amount of ether sent is correct, and then transfer the ether, then mint LP tokens according to the liquidity provided.
+
+        // Summary, 
         return liquidity;
     }
-
-    function removeLiquidity(uint _amount) public returns (uint, uint) {
+    function removeLiquidity(uint _amount) public returns (uint, uint) { // _amount if LP tokens.
         require(_amount > 0, "_amount should be greater than 0");
         uint ethReserve = address(this).balance;
         uint _totalSupply = totalSupply();
 
-        uint ethAmount = (ethReserve * _amount)/ _totalSupply;
+        uint ethAmount = (ethReserve * _amount)/ _totalSupply; // Total supply of ether.
 
-        uint cryptoDevTokenAmount = (getReserve() * _amount)/ _totalSupply;
+        uint cryptoDevTokenAmount = (getReserve() * _amount)/ _totalSupply; // What the amount is multiplied by dosen't matter, since the ratio is one to one.
 
-        _burn(msg.sender, _amount);
-        payable(msg.sender).transfer(ethAmount);
-        ERC20(cryptoDevTokenAddress).transfer(msg.sender, cryptoDevTokenAmount);
-        return (ethAmount, cryptoDevTokenAmount);
-    }
+        _burn(msg.sender, _amount); // burn LP tokens.
+        payable(msg.sender).transfer(ethAmount); // Transfer the amount of ether to the user.
+        ERC20(cryptoDevTokenAddress).transfer(msg.sender, cryptoDevTokenAmount); // Transfer the amount of CDTs to the user.
+        return (ethAmount, cryptoDevTokenAmount); // Return the amount numbers (For display? WHY?)
+    } 
+    // This is withdrawing the liquidity from both sides - 
 
     function getAmountOfTokens(
-        uint256 inputAmount,
-        uint256 inputReserve,
+        uint256 inputAmount, // Tokens that the user wants to buy, or already has bought
+        uint256 inputReserve, // 
         uint256 outputReserve
     ) public pure returns (uint256) {
-        require(inputReserve > 0 && outputReserve > 0, "Invalid Reserves");
-        uint256 inputAmountWithFee = inputAmount * 99;
-        uint256 numerator = inputAmountWithFee * outputReserve;
+        require(inputReserve > 0 && outputReserve > 0, "Invalid Reserves");  // Check and make sure that there are reserves.
+
+        uint256 inputAmountWithFee = inputAmount * 99; // Input * 99
+        uint256 numerator = inputAmountWithFee * outputReserve; // numerator is the input with the fee times the output Reserve
         uint256 denominator = (inputReserve * 1000) + inputAmountWithFee;
         return numerator / denominator;
-    }
+    } // The basic math is that we multiply * 99 and then divide by 100 to get the amount with the 1% fee subtracted.
+    // This is the formula Δy = (y*Δx)/(x + Δx); 
+    // x = inputReserve, y = outputReserve.
+
 
     function ethToCryptoDevToken(uint _minTokens) public payable {
         uint256 tokenReserve = getReserve();
