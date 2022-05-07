c8118ab29ca2d669555e4a875e0ab086cad3996b
nslee333 <nslee333@gmail.com>
2022-05-07 09:54:49 -0700

    Commit

--- Defi-exchange/hardhat-tutorial/contracts/Exchange.sol
+++ Defi-exchange/hardhat-tutorial/contracts/Exchange.sol
@@ -63,6 +63,40 @@ contract Exchange is ERC20 {
         return (ethAmount, cryptoDevTokenAmount);
     }
 
+    function getAmountOfTokens(
+        uint256 inputAmount,
+        uint256 inputReserve,
+        uint256 outputReserve
+    ) public pure returns (uint256) {
+        require(inputReserve > 0 && outputReserve > 0, "Invalid Reserves");
+        uint256 inputAmountWithFee = inputAmount * 99;
+        uint256 numerator = inputAmountWithFee * outputReserve;
+        uint256 denominator = (inputReserve * 1000) + inputAmountWithFee;
+        return numerator / denominator;
+    }
+
+    function ethToCryptoDevToken(uint _minTokens) public payable {
+        uint256 tokenReserve = getReserve();
+        uint256 tokensBought = getAmountOfTokens(
+            msg.value,
+            address(this).balance - msg.value,
+            tokenReserve
+        );
+
+        require(tokensBought >= _minTokens, "Insufficient output amount");
+        ERC20(cryptoDevTokenAddress).transfer(msg.sender, tokensBought);
+    }
+
+    function CryptoDevTokenToEth(uint _tokensSold, uint _minEth) public {
+        uint256 tokenReserve = getReserve();
+        uint256 ethBought = getAmountOfTokens(
+            _tokensSold,
+            tokenReserve,
+            address(this).balance;
+        );
+        require(ethBought >= _minEth, "Insufficient output amount");
+        ERC20(cryptoDevTokenAddress).transfer(ethBought); 
+    }
 
 
 }
