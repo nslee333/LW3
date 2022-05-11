325cb0d6d2029c707c3a0606c7c197e8a996991f
nslee333 <nslee333@gmail.com>
2022-05-11 07:45:34 -0700

    Commit

---
+++ Defi-exchange/my-app/utils/addLiquidity.js
@@ -0,0 +1,49 @@
+import { Contract, utils } from "ethers";
+import {
+    EXCHANGE_CONTRACT_ABI,
+    EXCHANGE_CONTRACT_ADDRESS,
+    TOKEN_CONTRACT_ABI,
+    TOKEN_CONTRACT_ADDRESS
+} from "../constants";
+import { tokenInstance, exchangeInstance } from "./utils";
+
+
+
+export const addLiquidity = async (
+    signer,
+    addCdAmountWei,
+    addEtherAmountWei
+) => {
+    try {
+        
+        const tokenContract = await tokenInstance(signer);
+        const exchangeContract = await exchangeInstance(signer);
+
+        let tx = await tokenContract.approve(
+            EXCHANGE_CONTRACT_ADDRESS,
+            addCdAmountWei.toString()
+        );
+        await tx.wait();
+
+        tx = await exchangeContract.addLiquidity(addCdAmountWei, {
+            value: addEtherAmountWei,
+        });
+        await tx.wait();
+    } catch (error) {
+        console.error(err);
+    }
+
+};
+
+export const calculateCd = async (
+    _addEther = "0",
+    etherBalanceContract,
+    cdTokenReserve
+) => {
+    const _addEtherAmountWei = utils.parseEther(_addEther);
+
+    const cryptoDevTokenAmount = _addEtherAmountWei
+        .mul(cdTokenReserve);
+        .div(etherBalanceContract);
+    return cryptoDevTokenAmount;
+};
