2d214b7f63f77d74ab1b1cb097cd91baec785ec2
nslee333 <nslee333@gmail.com>
2022-05-17 16:57:44 -0700

    Commit

--- Defi-exchange/my-app/utils/swap.js
+++ Defi-exchange/my-app/utils/swap.js
@@ -7,7 +7,7 @@ _swapAmountWei,
 provider,
 ethSelected,
 ethBalance,
-reservedCd,
+reservedCd
 ) => {
     const exchangeContract = exchangeInstance(provider);
 
@@ -20,7 +20,8 @@ reservedCd,
             ethBalance,
             reservedCd,
         );
-    } else { // This is the function but flipped, the amount to be swapped, with the CD tokens to be swapped and the CD reserve, and the eth balance, with the returned amount 
+    } else { // This is the function but flipped, the amount to be swapped, with the CD tokens to be swapped and the CD reserve, and the eth balance, with the returned amount of 
+        // Eth tokens to be recieved.
         amountOfTokens = await exchangeContract.getAmountOfTokens(
             _swapAmountWei,
             reservedCd,
@@ -28,9 +29,32 @@ reservedCd,
         )
     }
 
+    return amountOfTokens;
+
 }
 
-export const swapTokens = async () => {
+export const swapTokens = async (
+    signer,
+    swapAmountWei,
+    tokensToBeReceivedAfterSwap,
+    ethSelected
+) => {
+    const exchangeContract = await exchangeInstance(signer);
+    const tokenContract = await tokenInstance(signer);
+    let tx;
 
-
+    if (ethSelected) {
+        tx = await exchangeContract.ethToCryptoDevToken(
+            tokensToBeReceivedAfterSwap,
+            {
+                value: swapAmountWei,
+            }
+        );
+    } else {
+        tx = await exchangeContract.cryptoDevTokenToEth(
+            swapAmountWei,
+            tokensToBeReceivedAfterSwap
+        );
+    }
+    await tx.wait();
 }
