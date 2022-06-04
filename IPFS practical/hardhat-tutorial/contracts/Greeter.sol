72eac440c9351b5bb4c81c144445b27df56ca45e
Nathan <nslee333@gmail.com>
2022-06-04 10:55:52 -0700

    Commit

---
+++ IPFS practical/hardhat-tutorial/contracts/Greeter.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract LW3Punks is ERC721Enumerable, Ownable {
+
+    using Strings for uint256;
+
+    string _baseTokenURI;
+
+    uint256 public _price = 0.01 ether;
+
+    bool public _paused;
+
+    uint256 public maxTokenIds = 10;
+
+    uint256 public tokenIds;
+
+    modifier onlyWhenNotPaused {
+        require(!_paused, "Contract currently paused");
+        _;
+    }
+
+    constructor(string memory baseURI) ERC721("LW3Punks", "LW3P") {
+        _baseTokenURI = baseURI;
+    }
+
+    function mint() public payable onlyWhenNotPaused {
+        require(tokenIds < maxTokenIds, "Exceed Maximum LW3Punks supply");
+        require(msg.value >= _price, "Ether sent is not correct.");
+        tokenIds += 1;
+        _safeMint(msg.sender, tokenIds);
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return _baseTokenURI;
+    }
+
+    function setPaused(bool val) public onlyOwner {
+        _paused = val;
+    }
+
+    function withdraw() public onlyOwner {
+        address _owner = owner();
+        uint256 amount = address(this).balance;
+        (bool, sent) - _owner.call{value: amount}("");
+        require(sent, "Failed to send ether");
+    }
+
+    receive() external payable {}
+
+    fallback() external payable {}
+
+}
