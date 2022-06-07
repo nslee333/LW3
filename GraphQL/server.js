ebc1d88f292d5edb62380307481753d6cfc6e02e
Nathan <nslee333@gmail.com>
2022-06-06 18:31:31 -0700

    Commit

--- GraphQL/server.js
+++ GraphQL/server.js
@@ -4,7 +4,7 @@ const app = express();
 
 const {
     GraphQLSchema,
-    GraphQLObjectType,
+    GraphQLObjectType, 
     GraphQLString,
     GraphQLList,
     GraphQLInt,
@@ -17,7 +17,10 @@ const {
     // If we don't do this, then the server will crash.
 
 
-const authors = [
+
+
+// Arrays that will act as our database.
+const authors = [ 
 	{ id: 1, name: 'J. K. Rowling' },
 	{ id: 2, name: 'J. R. R. Tolkien' },
 	{ id: 3, name: 'Brent Weeks' }
@@ -34,7 +37,8 @@ const books = [
 	{ id: 8, name: 'Beyond the Shadows', authorId: 3 }
 ]
 
-
+// This is defining a Book Type object. 
+// Fields are our properties of an instance of a book object.
 const BookType = new GraphQLObjectType({
     name: 'Book',
     description: 'This represents a book written by an author',
@@ -50,6 +54,7 @@ const BookType = new GraphQLObjectType({
     })
 })
 
+// Defining an author type object.
 
 const AuthorType = new GraphQLObjectType({
     name: 'Author',
@@ -66,89 +71,92 @@ const AuthorType = new GraphQLObjectType({
 
 
 
+// This is the function that helps us make our queries.
 
 
 
 const RootQueryType = new GraphQLObjectType({
     name: 'Query',
     description: 'Root Query',
-    fields: () => ({
-        book: {
-            type: BookType,
-            description: 'A Single Book',
-            args: {
+    fields: () => ({ // We define it with a function to ensure that the functions inside these queries are defined before they're called.
+        book: { // A query for a single book. 
+            type: BookType, // the type.
+            description: 'A Single Book', 
+            args: { // Arguments that we'll give the query.
                 id: { type: GraphQLInt }
             },
-            resolve: (parent, args) => books.find(book => book.id === args.id)
+            resolve: (parent, args) => books.find(book => book.id === args.id) // Resolution - parent isn't used, it takes the args, then uses the (type).find GraphQL function,
+            // then it returns the book id that matches the arg id.
         },
-        books: {
+        books: { // This is for all the books in the array. It returns a list instead of a single booktype object.
             type: new GraphQLList(BookType),
             description: 'List of books',
             resolve: () => books
         },
-        authors: {
+        authors: { // Returns a list of authors. 
             type: new GraphQLList(AuthorType),
             description: 'List of all authors',
             resolve: () => authors
         },
-        author: {
+        author: { // Returns a single author.
             type: AuthorType,
             description: 'A single author',
             args: {
                 id: { type: GraphQLInt }
             },
-            resolve: (parent, args) => authors.find(author => author.id === author.id)
+            resolve: (parent, args) => authors.find(author => author.id === args.id) // Return the author object that matches the args id. 
         },
     }) // Whenever you wrap the parentheses with squiggly brackets, it automatically returns it so we don't have to write a return statement.
 })
 
-const RootMutationType = new GraphQLObjectType({
-    name: 'Mutation',
+const RootMutationType = new GraphQLObjectType({ // This is used for modifying data in a database.
+    name: 'Mutation', // mutation is a keyword used for modifying data in a database.
     description: 'Root Mutation',
     fields: () => ({
-        addBook: {
+        addBook: { // This function is used to add a book to the database.
             type: BookType,
             description: "Add a book",
-            args: {
-                name: { type: GraphQLNonNull(GraphQLString) },
+            args: { // Arguments used to create the new book.
+                name: { type: GraphQLNonNull(GraphQLString) }, 
                 authorId: { type: GraphQLNonNull(GraphQLInt)}
             },
-            resolve: (parent, args ) => {
-                const book =  { id: books.length + 1, name: args.name, authorId: args.authorId }
-                books.push(book)
-                return book
+            resolve: (parent, args ) => { 
+                const book =  { id: books.length + 1, name: args.name, authorId: args.authorId } // Assigning 'book' to this object, it adds 1 to the length of books, inputs in
+                // the args name as the name, and the Args authorId as the author.
+                books.push(book) // Push that newly created book object to the array (or database).
+                return book // Return book.
             },
         },
-            addAuthor: {
-                type: AuthorType,
-                description: "Add an Author",
-                args: {
-                    name: { type: GraphQLNonNull(GraphQLString) }
-                },
-                resolve: (parent, args ) => {
-                    const author =  { id: authors.length + 1, name: args.name, authorId: args.authorId }
-                    authors.push(author)
-                    return author
-                }
+        addAuthor: { // Adding an author.
+            type: AuthorType,
+            description: "Add an Author",
+            args: {
+                name: { type: GraphQLNonNull(GraphQLString) } // Args for the name of the author.
+            },
+            resolve: (parent, args ) => { // Same as the structure above.
+                const author =  { id: authors.length + 1, name: args.name, authorId: args.authorId }
+                authors.push(author)
+                return author
             }
+        }
     })
 })  
 
 
 
-const schema = new GraphQLSchema({
-    query: RootQueryType,
-    mutation: RootMutationType
+const schema = new GraphQLSchema({ // This schema is used to tell GraphIQL what is what so that it works GraphQL.
+    query: RootQueryType, // Telling GraphQL how to treat our RootQueryType, which is our Query function.
+    mutation: RootMutationType // Telling GraphQL how to treat our RootMutationType, which is our Mutation function. Mutation is used to modify content in the database.
 })
 
 
-app.use('/graphql', graphqlHTTP({
+app.use('/graphql', graphqlHTTP({ // This is the config on how to set up the GraphQL / GraphIQL.
     schema: schema,
     graphiql: true
 }))
 
 
-app.listen(5000., () => console.log("Server running"))
+app.listen(5000., () => console.log("Server running")) // This is just something to make sure that the server is running.
 
 
 
