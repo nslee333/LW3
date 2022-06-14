92c93778048c51724294e7e47e11a64163dcc3b7
Nathan <nslee333@gmail.com>
2022-06-13 17:05:37 -0700

    Commit

--- ReEntrancyAttack/contracts/ReentrancyGuard.sol
+++ ReEntrancyAttack/contracts/ReentrancyGuard.sol
@@ -4,12 +4,10 @@ pragma solidity ^0.8.4;
 contract ReentrancyGuard {
 
     modifier nonReentrant() {
-        require(!locked, "No re-Entrancy")
+        require(!locked, "No re-Entrancy");
         locked = true;
         _;
         locked = false;
     }
-
-
 
 }
