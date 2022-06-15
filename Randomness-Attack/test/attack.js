bf309ee70ff6ca7fdcd6b8c6b1dd9d87633b987c
Nathan <nslee333@gmail.com>
2022-06-14 18:37:05 -0700

    Commit

--- Randomness-Attack/test/attack.js
+++ Randomness-Attack/test/attack.js
@@ -10,25 +10,16 @@ describe("Attack", function () {
     const _game = await Game.deploy({ value: utils.parseEther("0.1")});
     await _game.deployed();
 
-    const balanceGame = await _game.getBalance();
-
     console.log("Game Contract Deployed to:", _game.address);
-    console.log(balanceGame);
 
     const Attack = await ethers.getContractFactory("Attack"); // Deployed the Attack contract.
     const _attack = await Attack.deploy(_game.address);
-
-    console.log(balanceGame);
-
     console.log("Attack contract address", _attack.address); 
-
-    console.log(balanceGame);
 
     const tx = await _attack.attack(); // Calling the attack function.
     await tx.wait();
 
-    console.log(balanceGame);
-
+    
     const _balanceGame = await _game.getBalance();
     // const balanceGame = await _game.getBalance();
     expect(_balanceGame).to.equal(BigNumber.from("0")); // Expect the balance of the game to equal zero.
