f9c072d26b890e10100df2ba7721d6aa08432554
Nathan <nslee333@gmail.com>
2022-06-16 20:17:08 -0700

    Commit

--- Meta-Transactions/contracts/MetaTokenSender.sol
+++ Meta-Transactions/contracts/MetaTokenSender.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
-contract RandomTokne is ERC20 {
-    constructor() ERC20("",""); {}
+contract RandomToken is ERC20 {
+    constructor() ERC20("","") {}
     
     function freeMint(uint amount) public {
         _mint(msg.sender, amount);
@@ -16,17 +16,19 @@ contract TokenSender {
     using ECDSA for bytes32;
 
     function transfer(address sender, uint amount, address recipient, address tokenContract, bytes memory signature) public {
-        bytes32 messageHash = getHash(sender, amount, recipient, tokenContract);
-        bytes32 signedMessageHash = messageHash.toEthSignedMessageHash();
+        bytes32 messageHash = getHash(sender, amount, recipient, tokenContract); // calling the GetHash function defined below.
+        bytes32 signedMessageHash = messageHash.toEthSignedMessageHash(); // This converts to an Ethereum signed hash, this is used for interoperability with EIP191.
 
-        address signer = signedMessageHash.recover(signature);
+        address signer = signedMessageHash.recover(signature); // This is the signed message hash
 
         require(signer == sender, "Signature does not come from sender");
 
         bool sent = ERC20(tokenContract).transferFrom(sender, recipient, amount);
         require(sent, "Transfer Failed");
     }
-    function getHash(address sender, uint amount, address recipient, address tokenContract) public pure returns (bytes32) {
+
+    // This function takes in the below arguments, and ABI encodePacks them and then hashes those values, this is called by the transfer function.
+    function getHash(address sender, uint amount, address recipient, address tokenContract) public pure returns (bytes32) { 
         return keccak256(abi.encodePacked(sender, amount, recipient, tokenContract));
     }
 }
---
+++ Meta-Transactions/test/metatxn-test.js
@@ -0,0 +1,74 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const { BigNumber } = require("ethers");
+const { arrayify, parseEther } = require("ethers/lib/utils");
+
+describe("MetaTokenTransfer", function () {
+    it("Should let user transfer tokens through a receiver", async function () {
+
+        const RandomTokenFactory = await ethers.getContractFactory("RandomToken"); // Deploy a random token contract.
+        const randomTokenContract = await RandomTokenFactory.deploy();
+        await randomTokenContract.deployed();
+
+        const MetaTokenSenderFactory = await ethers.getContractFactory("TokenSender"); // Deploy a meta token sender contract.
+        const tokenSenderContract = await MetaTokenSenderFactory.deploy();
+        await tokenSenderContract.deployed();
+
+        const [_, userAddress, relayerAddress, recipientAddress ] = await ethers.getSigners(); // Get some signers.
+        await ethers.getSigners();
+
+        const tenThousandTokensWiithDecimals = parseEther("10000"); // Get 10,000 ether for testing.
+        const userTokenContractInstance = randomTokenContract.connect(userAddress); // This is creating an instance of random token contract that is connected to an address.
+        const mintTxn = await userTokenContractInstance.freeMint( // call the freeMint function on the random token contract that is connected to an address, and 
+        // Mint 10,000 ether.
+            tenThousandTokensWiithDecimals
+        );
+
+        await mintTxn.wait(); // Wait for the transaction to process.
+
+        // This is giving userTokenContractInstance infinite approval for transferring the tokens.
+        const approveTxn = await userTokenContractInstance.approve( 
+            tokenSenderContract.address,
+            BigNumber.from(
+                "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+            )
+        );
+        await approveTxn.wait();
+
+
+        const transferAmountOfTokens = parseEther("10"); // Getting 10 ether.
+        const messageHash = await tokenSenderContract.getHash( // Getting the message hash for the transaction.
+            userAddress.address,
+            transferAmountOfTokens,
+            recipientAddress,
+            randomTokenContract.address
+        );
+        const signature = await userAddress.signMessage(arrayify(messageHash)); // This is prompting the user to sign the message to transfer the 10 tokens.
+
+
+
+        const relayerSenderContractInstance = tokenSenderContract.connect(relayerAddress); // Getting a relayer contract instance.
+
+        const metaTxn = await relayerSenderContractInstance.transfer( // This is the meta transaction being called on the relayerSenderContractInstance
+            userAddress.address,
+            transferAmountOfTokens,
+            recipientAddress,
+            randomTokenContract.address,
+            signature
+        );
+        await metaTxn.wait();
+        
+        const userBalance = await randomTokenContract.balanceOf( // Getting the balance of the user.
+            userAddress.address,
+        );
+
+        const recipientBalance = await randomTokenContract.balanceOf( // Getting the balance of the recipientAddress.
+            recipientAddress.address
+        );
+
+        expect(userBalance.lt(tenThousandTokensWiithDecimals)).to.be.true; // Expect asserts that the userBalance is less than 10,000 tokens to be true.
+        expect(recipientBalance.gt(BigNumber.from(0))).to.be.true; // Expects that the recipient address's balance is greater than 0, to be true.
+
+
+    })
+})
