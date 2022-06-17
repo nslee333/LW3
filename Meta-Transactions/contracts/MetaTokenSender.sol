98a8a7fbeefdedf288972465b0d1d6686f3da1be
Nathan <nslee333@gmail.com>
2022-06-16 20:36:58 -0700

    Commit

--- Meta-Transactions/contracts/MetaTokenSender.sol
+++ Meta-Transactions/contracts/MetaTokenSender.sol
@@ -15,20 +15,28 @@ contract RandomToken is ERC20 {
 contract TokenSender {
     using ECDSA for bytes32;
 
-    function transfer(address sender, uint amount, address recipient, address tokenContract, bytes memory signature) public {
-        bytes32 messageHash = getHash(sender, amount, recipient, tokenContract); // calling the GetHash function defined below.
+    mapping(bytes32 => bool) executed; // This is used to make sure that the transaction only happens once, and that it isn't vulnerable to a replay signature attack.
+
+    // We add the nonce to help keep every transaction unique, since our hasing algorithms are deterministic, we have to introduce an element of randomness to
+    // make sure every transaction is unique.
+    function transfer(address sender, uint amount, address recipient, address tokenContract, uint nonce, bytes memory signature) public {
+        bytes32 messageHash = getHash(sender, amount, recipient, nonce, tokenContract); // calling the GetHash function defined below.
         bytes32 signedMessageHash = messageHash.toEthSignedMessageHash(); // This converts to an Ethereum signed hash, this is used for interoperability with EIP191.
+
+        require(!executed[signedMessageHash], "Already Executed"); // Make sure that this signature hasn't already been executed.
 
         address signer = signedMessageHash.recover(signature); // This is the signed message hash
 
         require(signer == sender, "Signature does not come from sender");
 
+
+        executed[signedMessageHash] = true; // Mark it as executed.
         bool sent = ERC20(tokenContract).transferFrom(sender, recipient, amount);
         require(sent, "Transfer Failed");
     }
 
     // This function takes in the below arguments, and ABI encodePacks them and then hashes those values, this is called by the transfer function.
-    function getHash(address sender, uint amount, address recipient, address tokenContract) public pure returns (bytes32) { 
+    function getHash(address sender, uint amount, address recipient, unit nonce, address tokenContract) public pure returns (bytes32) { 
         return keccak256(abi.encodePacked(sender, amount, recipient, tokenContract));
     }
 }
