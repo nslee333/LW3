b9f358567bc5c38a009b92d0788e9e0da0fa399f
Nathan <nslee333@gmail.com>
2022-06-18 11:09:54 -0700

    Commit

---
+++ NFT_OZ/Code Walkthrough/Readme.txt
@@ -0,0 +1,17 @@
+Hey there, this is my code Walkthrough of the code that's in this file.
+
+I'm doing this for my own learning as well as anyone who is reading this, I try to 
+explain the concepts / code that I don't understand to myself in this txt file.
+
+Typicall I have the Txt file on the right side of the screen in VS code, with the source
+code on the left side.
+
+The format for the txt file is:
+
+Code that I'm explaining
+    - General idea of the code.
+    - The syntax if I can find the documentation for it.
+    - Any other things I'm not sure about.
+If there is any code I have explained before I'll list it, then say that I understand what its 
+doing.
+
---
+++ NFT_OZ/scripts/deploy.js
@@ -0,0 +1,36 @@
+
+const { ethers } = require("hardhat");
+
+
+
+// 
+
+async function main() { 
+
+
+    const nftContract = await ethers.getContractFactory("NateNFT");
+
+    // Assigning variable name to contract factory (ethers) for 'NateNFT'
+
+    const deployedNFTContract = await nftContract.deploy();
+
+    // Variable name assigned to await deployed contract.
+
+    console.log("NFT Contract Address:", deployedNFTContract.address);
+
+    // Log contract address.
+
+}
+
+main()
+    .then(() => process.exit(0))
+    // If issue then exit?
+    .catch((error) => {
+        // If catch issue, log issue then exit.
+        console.error(error);
+        // Log error report.
+        process.exit(1);
+        // Exit process.
+    });
+
+    // This is how to handle async errors.
